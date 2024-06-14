be89377f3f7b02fa19438b957004293ae362093d
haoking1111 <ngbao08052003@gmail.com>
2024-06-14 18:42:19 +0700

    login

--- lib/presentation/screens/auth/login/login.dart
+++ lib/presentation/screens/auth/login/login.dart
@@ -11,6 +11,45 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      color: MyColors.primaryColor,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                Image.asset(
+                  MyAssets.mainLogo,
+                  width: 139.w,
+                  height: 42.h,
+                ).centered(),
+          
+                100.h.heightBox,
+          
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(36),
+                          topRight: Radius.circular(36)
+                      )
+                  ),
+                  child: Column(
+                    children: [
+                      50.h.heightBox,
+                      'Login'.text.color(MyColors.primaryColor).make()
+                    ],
+                  ),
+                ),
+          
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/screens/auth/login/login_imports.dart
+++ lib/presentation/screens/auth/login/login_imports.dart
@@ -1,5 +1,11 @@
 import 'package:auto_route/annotations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_journo_blog_app/core/contants/my_assets.dart';
+import 'package:flutter_journo_blog_app/core/contants/my_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 part 'login.dart';
 part 'login_view_model.dart';
