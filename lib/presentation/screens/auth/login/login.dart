36760fb4d16d1a255f782db5d25bc0ce52fdf2e4
haoking1111 <ngbao08052003@gmail.com>
2024-06-15 11:20:28 +0700

    Login Ui commit

--- lib/presentation/screens/auth/login/login.dart
+++ lib/presentation/screens/auth/login/login.dart
@@ -11,42 +11,105 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: MyColors.primaryColor,
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: [
-                Image.asset(
-                  MyAssets.mainLogo,
-                  width: 139.w,
-                  height: 42.h,
-                ).centered(),
-          
-                100.h.heightBox,
-          
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(36),
-                          topRight: Radius.circular(36)
-                      )
-                  ),
+    return Scaffold(
+      backgroundColor: MyColors.primaryColor,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Image.asset(
+                MyAssets.mainLogo,
+                width: 139.w,
+                height: 42.h,
+              ).centered(),
+              100.h.heightBox,
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(36),
+                        topRight: Radius.circular(36))),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.w),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       50.h.heightBox,
-                      'Login'.text.color(MyColors.primaryColor).make()
+                      'Login'
+                          .text
+                          .size(18)
+                          .color(MyColors.primaryColor)
+                          .fontWeight(FontWeight.w700)
+                          .makeCentered(),
+                      48.h.heightBox,
+                      'Email'.text.make(),
+                      8.h.heightBox,
+                      const VxTextField(
+                        fillColor: Colors.transparent,
+                        borderColor: MyColors.primaryColor,
+                        borderType: VxTextFieldBorderType.roundLine,
+                        borderRadius: 10,
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      20.h.heightBox,
+                      'Password'.text.make(),
+                      8.h.heightBox,
+                      const VxTextField(
+                        isPassword: true,
+                        obscureText: true,
+                        fillColor: Colors.transparent,
+                        borderColor: MyColors.primaryColor,
+                        borderType: VxTextFieldBorderType.roundLine,
+                        borderRadius: 10,
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      40.h.heightBox,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 220,
+                            child: CheckboxListTile(
+                              value: false,
+                              onChanged: (value) {},
+                              title: 'Remember Me'.text.make(),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              contentPadding: EdgeInsets.zero,
+                            ),
+                          ),
+                          Expanded(
+                            child: 'Forgot Password'
+                                .text
+                                .size(14)
+                                .align(TextAlign.center).make()
+                          ),
+                        ],
+                      ),
+                      40.h.heightBox,
+                      PrimaryButton(
+                        title: 'Login',
+                        onPressed: () {},
+                      ),
+                      40.h.heightBox,
+                      'Don\'t have an account '
+                          .richText
+                          .size(14)
+                          .color(MyColors.primaryColor)
+                          .withTextSpanChildren([
+                        const TextSpan(
+                          text: 'Sign Up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ]).makeCentered(),
                     ],
                   ),
                 ),
-          
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/presentation/screens/auth/login/login_imports.dart
+++ lib/presentation/screens/auth/login/login_imports.dart
@@ -1,9 +1,8 @@
 import 'package:auto_route/annotations.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_journo_blog_app/core/contants/my_assets.dart';
 import 'package:flutter_journo_blog_app/core/contants/my_colors.dart';
+import 'package:flutter_journo_blog_app/presentation/common_widgets/common_widget_imports.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:velocity_x/velocity_x.dart';
 
