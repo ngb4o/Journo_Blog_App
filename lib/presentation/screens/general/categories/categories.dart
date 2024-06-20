c0b46faf3831a4de13eddfd95f9df63c05c38e2d
ngb4o <ngbao08052003@gmail.com>
2024-06-20 09:51:57 +0700

    appbar

--- lib/presentation/screens/general/categories/categories.dart
+++ lib/presentation/screens/general/categories/categories.dart
@@ -12,18 +12,19 @@ class _CategoriesState extends State<Categories> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: 'Categories'.text.color(Colors.white).makeCentered(),
+        title: 'Categories'.text.color(Colors.white).make(),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
         backgroundColor: MyColors.primaryColor,
         actions: const [
           Padding(
-            padding: const EdgeInsets.only(right: 8.0),
+            padding: EdgeInsets.only(right: 8.0),
             child: Icon(
               Icons.add,
               color: Colors.white,
             ),
           ),
         ],
-
       ),
       body: Center(
         child: 'Screen categories'.text.make(),
